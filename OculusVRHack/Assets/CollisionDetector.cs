69fb327029fc9a7bc88dc38b51eb28e7324f97d4
David Zhang <David@Davids-MacBook-Pro.local>
2014-11-15 19:44:11 -0800

    Adding video

---
+++ OculusVRHack/Assets/CollisionDetector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Yo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) { 
+		Debug.Log (other.transform.name);
+	}
+}
